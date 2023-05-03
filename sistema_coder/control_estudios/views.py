a196b8e35149607fd35bf13cbda0b3c7e7f23fad
Martinrl1986 <kskta993@gmail.com>
2023-05-03 11:00:36 -0300

    Todo el proyecto subido

---
+++ sistema_coder/control_estudios/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+def inicio(request):
+    contexto = {}
+    
+
+
+def listar_estudiantes(request):
+    contexto = {
+        "estudiantes": [
+            {"nombre": "Emanuel", "apellido": "Dautel"},
+            {"nombre": "Manuela", "apellido": "Gomez"},
+            {"nombre": "Ivan", "apellido": "Tomasevich"},
+            {"nombre": "Carlos", "apellido": "Perez"},
+        ]
+    }
+    http_responde = render(
+        request=request,
+        template_name='control_estudios/lista_estudiantes.html',
+        context=contexto,
+    )
+    return http_responde
+
